7789b8539d25242024f395237b3390e79a657f84
Matt Keeter <matt@oxide.computer>
2022-11-21 13:33:24 -0500

    Add version::CURRENT

--- controller/src/lib.rs
+++ controller/src/lib.rs
@@ -297,7 +297,7 @@ impl Controller {
     // Return a header using the next available message ID.
     fn next_header(&self) -> Header {
         Header {
-            version: message::version::V2,
+            version: message::version::CURRENT,
             message_id: self.message_id.fetch_add(1, Ordering::SeqCst),
         }
     }
@@ -657,7 +657,7 @@ impl IoLoop {
                     probes::message__received!(|| (peer.ip(), &message));
 
                     // Sanity check the protocol version.
-                    if message.header.version != message::version::V2 {
+                    if message.header.version != message::version::CURRENT {
                         // If the version does not match, we're choosing to drop
                         // the packet rather than reply with a version mismatch
                         // error. Without a matching version, we can't really
@@ -666,7 +666,7 @@ impl IoLoop {
                         debug!(
                             self.log,
                             "deserialized message with incorrect version";
-                            "expected" => message::version::V2,
+                            "expected" => message::version::CURRENT,
                             "actual" => message.header.version,
                             "peer" => peer,
                         );
@@ -675,7 +675,7 @@ impl IoLoop {
                                 peer.ip(),
                                 format!(
                                     "incorrect version: expected {}, actual {}",
-                                    message::version::V2,
+                                    message::version::CURRENT,
                                     message.header.version,
                                 ),
                             )
--- messages/src/message.rs
+++ messages/src/message.rs
@@ -18,6 +18,8 @@ use serde::Serialize;
 pub mod version {
     pub const V1: u8 = 1;
     pub const V2: u8 = 2;
+
+    pub const CURRENT: u8 = V2;
 }
 
 /// A common header to all messages between host and SP.
