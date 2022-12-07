d059a59fcab8efb23897a538f2f1c95d88ca4d64
Aaron Hartwig <aaron@oxidecomputer.com>
2022-12-07 17:34:25 -0600

    bump protocol version

--- messages/src/message.rs
+++ messages/src/message.rs
@@ -18,8 +18,9 @@ use serde::Serialize;
 pub mod version {
     pub const V1: u8 = 1;
     pub const V2: u8 = 2;
+    pub const V3: u8 = 3;
 
-    pub const CURRENT: u8 = V2;
+    pub const CURRENT: u8 = V3;
 }
 
 /// A common header to all messages between host and SP.
